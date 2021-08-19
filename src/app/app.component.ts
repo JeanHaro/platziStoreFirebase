73d4c30944a00f274ca77f980ea35dc101f105c3
Jean Haro <JeanCHL98@hotmail.com>
2021-08-19 18:50:46 -0500

    String Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // la maquetación
   templateUrl: './app.component.html',
+  // archivos de estilos
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  // Variables serán renderizadas en ese template
+  // Se puede llamar desde el archivo app.component.html con String Interpolation
+  /* Las variables que tengamos acá, son variables que podemos mostrar o renderizar en nuestro archivo 
+  app.component.html */
   title = 'platzi-store';
 }
