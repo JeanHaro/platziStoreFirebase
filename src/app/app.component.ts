c326503a9fd230871653402b219e5057ac66041f
Jean Haro <JeanCHL98@hotmail.com>
2021-08-19 20:51:51 -0500

    Uso de ngFor add y delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,23 @@ export class AppComponent {
   /* Las variables que tengamos acá, son variables que podemos mostrar o renderizar en nuestro archivo 
   app.component.html */
   title = 'platzi-store';
+
+  // Array
+  items = ['Jean', 'Julian', 'Perez'];
+
+  // Metodo
+  addItem() {
+    // Hacer una referencia a nuestros Items
+    // push() es para añadir un nuevo elemento a nuestro Array
+    this.items.push('nuevo item');
+  }
+
+  // Eliminar un item
+  // Parámetro de entrada es la posición
+  deleteItem (index: number) {
+    // splice() - es para eliminar un elemento del array e indicandole la posición que queremos borrar
+    // primer parámetro se indica en que posición se empieza a borrar
+    // segundo elemento se indica cuantos elementos se quieren borrar
+    this.items.splice(index, 1);
+  }
 }
