7c5b9ddd415082583c2d7683181a18d6427a84b4
Jean Haro <JeanCHL98@hotmail.com>
2021-08-19 22:06:55 -0500

    Uso de ngFor para recorrer objetos

---
+++ src/app/product.model.ts
@@ -0,0 +1,8 @@
+// export - para que lo puedan utilizar los demás
+export interface Product {
+    id: string;
+    image: string;
+    title: string;
+    price: number;
+    description: string;
+}
