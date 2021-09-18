59d55ccaee032c38ea7274a058efab5168bee18e
Jean Haro <JeanCHL98@hotmail.com>
2021-09-17 23:47:47 -0500

    Cierre de ejercicio de detalle

--- src/app/admin/components/products-list/products-list.component.ts
+++ src/app/admin/components/products-list/products-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { ProductsService } from '../../../core/services/products/products.service'; 
 
+import { Product } from '../../../core/models/product.model';
+
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
@@ -9,20 +11,28 @@ import { ProductsService } from '../../../core/services/products/products.servic
 })
 export class ProductsListComponent implements OnInit {
 
-  products = [];
+  products!: Product[];
   // Esto se saca de Angular Material
   displayedColumns: string[] = ['id', 'title', 'price', 'actions'];
 
   constructor (private productsService: ProductsService) { }
 
   ngOnInit(): void {
+    this.fetchProducts();
   }
 
   // Para mandar datos
-  fetchProducts () {
+  fetchProducts(): void {
     this.productsService.getAllProducts()
-    .subscribe(products => {
-      this.products = this.products;
+    .subscribe((products) => {
+      this.products = products;
+    })
+  }
+
+  deleteProduct (id: string) {
+    this.productsService.deleteProduct(id)
+    .subscribe(rta => {
+      this.fetchProducts();
     })
   }
 }
