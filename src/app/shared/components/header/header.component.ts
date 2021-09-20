17c3a64a0d66b5d80790f139999f19563f8225dc
Jean Haro <JeanCHL98@hotmail.com>
2021-09-20 12:59:23 -0500

    Usa un pipe para hacer un contador de productos

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+// map - transformar el valor que nos llega por uno nuevo
+import { map } from 'rxjs/operators';
 
 // Servicio
 import { CartService } from '../../../core/services/cart.service';
@@ -10,14 +13,14 @@ import { CartService } from '../../../core/services/cart.service';
 })
 export class HeaderComponent implements OnInit {
 
-  total = 0;
+  // Observable de valor numero
+  total$: Observable<number>;
 
   constructor(private cartService: CartService) { 
     // Acá obtenemos los productos que se vayan agregando al carrito
-    this.cartService.cart$.subscribe(products => {
-      console.log(products);
-      this.total = products.length;
-    })
+    // En map, se sentencia como lo vamos a transformar a un valor que es el tamaño de la lista
+     // Ahora nos suscribimos, pero desde el template
+    this.total$ = this.cartService.cart$.pipe(map(products => products.length));
   }
 
   ngOnInit(): void {
