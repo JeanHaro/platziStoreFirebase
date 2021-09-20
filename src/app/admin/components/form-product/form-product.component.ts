d78a34c55c14296303552077b2e01a270b9525cc
Jean Haro <JeanCHL98@hotmail.com>
2021-09-20 01:40:33 -0500

    Ajustar estilos en un formulario

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 // FormGroup - es un grupo de controles
 // FormBuilder - es una extensión de angular, que nos sirve para crear ese grupo rápidamente
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { ProductsService } from '../../../core/services/products/products.service';
 
 @Component({
   selector: 'app-form-product',
@@ -12,11 +15,32 @@ export class FormProductComponent implements OnInit {
 
   form!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { 
+  constructor(
+    private formBuilder: FormBuilder, 
+    private productService: ProductsService, 
+    private router: Router
+  ) { 
     this.buildForm();
   }
 
   ngOnInit(): void {
+  }
+
+  saveProduct(event: Event) {
+    // preventDefault() - quite la acción por defecto, para que no recargue la página
+    event.preventDefault();
+
+    if (this.form.valid) {
+      const product = this.form.value;
+      this.productService.createProduct(product)
+      .subscribe((newProduct) => {
+        console.log(newProduct);
+        // Una ruta que le damos
+        this.router.navigate(['./admin/products'])
+      });
+    }
+
+    console.log(this.form.value);
   }
 
   private buildForm() {
