345cd66a6861c766848c0c556728250c2a3d1562
Jean Haro <JeanCHL98@hotmail.com>
2022-03-10 00:55:45 -0500

    Actualizando Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,12 @@ export const environment = {
   production: false,
   url_api: 'https://platzi-store.herokuapp.com',
   firebase: {
-    apiKey: 'AIzaSyD5MnTEwNv1EFX1XL4c67TLvExXynCv1LI',
-    authDomain: 'platzi-store-a1a34.firebaseapp.com',
-    projectId: 'platzi-store-a1a34',
-    storageBucket: 'platzi-store-a1a34.appspot.com',
-    messagingSenderId: '687628647715',
-    appId: '1:687628647715:web:5e2ecc080a86f20b9a6096'
+    apiKey: "AIzaSyD5MnTEwNv1EFX1XL4c67TLvExXynCv1LI",
+    authDomain: "platzi-store-a1a34.firebaseapp.com",
+    projectId: "platzi-store-a1a34",
+    storageBucket: "platzi-store-a1a34.appspot.com",
+    messagingSenderId: "687628647715",
+    appId: "1:687628647715:web:5e2ecc080a86f20b9a6096"
   }
 };
 
